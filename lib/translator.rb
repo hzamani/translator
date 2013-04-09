e714e6c480a54faada9a0eca4312b94387d4a6fa
Hassan Zamani <hsn.zamani@gmail.com>
2013-04-09 23:05:57 +0430

    rails plugin new

---
+++ lib/translator.rb
@@ -0,0 +1,2 @@
+module Translator
+end
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'd6fed0b483dd67716b5639efe2f417bb57bd3b44a2a3660ba8f0e568d7bf4d544636ec67a8a5568b11f95ccf85421fa10c41e3cfb2bbd1b3382b530d93ec53af'
